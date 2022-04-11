4ed6d6374f49e2f24cb564dc65fa35f10f1a4023
carlosvico <calladrianjr@gmail.com>
2022-04-10 22:07:59 -0300

    implementado rotas para listagem de usuarios e busca por id

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 [Arnaldo Neto](https://github.com/acn3to)
 
 [Carlos Vieira](https://github.com/carlosvico)
+
+
+
+
+
+
+<p align="center">
+<img src="http://img.shields.io/static/v1?label=STATUS&message=EM%20DESENVOLVIMENTO&color=GREEN&style=for-the-badge"/>
+</p>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,44 @@
-const { Router } = require("express");
+const { Router, application } = require("express");
 
 const routes = new Router();
+
+const users = [
+  {id: 1, "nome": "Homer Simpson", "email": "hsimpsom@teste.com"},
+  {id: 2, "nome": "Will Smith", "email": "wsimith@teste.com"},
+  {id: 3, "nome": "Sandro Botticellli", "email": "sbotti@teste.com"},
+  {id: 4, "nome": "Raimundo Nonato", "email": "rnonato@teste.com"}
+]
 
 routes.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
+
+
+
+
+/* -----------------------------
+        Listando usuarios
+--------------------------------*/
+routes.get('/users', (req, res) => {
+  res.status(200).json(users);
+})
+
+/* -----------------------------
+        Pega usuarios por Id
+--------------------------------*/
+routes.get('/user/:id', (req, res) => {
+  let index = searchUser(req.params.id);
+  res.json(users[index]);
+});
+
+
+
+/* ---------------------------------------------
+        Função que percorre o array de usuarios
+             e localiza um pelo id
+-----------------------------------------------*/
+function searchUser(id) {
+  return users.findIndex(user => user.id == id);
+}
 
 module.exports = routes;
