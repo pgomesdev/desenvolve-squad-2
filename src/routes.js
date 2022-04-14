02724bdf44fc7fde34771d4980628eeb8e739fd9
valdirapn <valdiraires.pn@gmail.com>
2022-04-14 16:16:08 -0300

    feat: create user

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
+const UserController = require('./controllers/UserController')
 
 const routes = new Router();
 
 routes.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
+
+routes.post("/users", UserController.createUser)
 
 module.exports = routes;
