d522310ae0f7e614061b5d6c1bdf893f9e7f2a20
valdirapn <valdiraires.pn@gmail.com>
2022-04-14 16:16:08 -0300

    feat: create user

---
+++ src/controllers/UserController.js
@@ -0,0 +1,16 @@
+const db = require('../models')
+
+class UserController {
+  static async createUser(req, res) {
+    const user = req.body
+
+    try {
+      const createdUser = await db.Users.create(user)
+      return res.status(200).json(createdUser)
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+}
+
+module.exports = UserController
