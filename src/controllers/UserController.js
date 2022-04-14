a4bf063849e7a0729437bf36827cdcaf0845ba67
valdirapn <valdiraires.pn@gmail.com>
2022-04-14 16:33:42 -0300

    update: add json midleware to express

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,6 +4,9 @@ class UserController {
   static async createUser(req, res) {
     const user = req.body
 
+    console.log(req)
+    console.log(user)
+
     try {
       const createdUser = await db.Users.create(user)
       return res.status(200).json(createdUser)
